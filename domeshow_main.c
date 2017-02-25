dcf4a003f652a8ef81298c8c933603c3c2eb2432
Ian Smith <smit7356@umn.edu>
2017-02-24 23:28:53 -0600

    Adding DMX read. Maybe?

--- domeshow_main.c
+++ domeshow_main.c
@@ -1,16 +1,27 @@
 /*
  * File:   domeshow_main.c
- * Author: Dan
+ * Author: Dan Sunderman, Ian Smith
  *
  * Created on January 24, 2017, 12:59 PM
+ * 
+ * Some implementation ideas based on Phosphorus by John Bisgaard.
+ * 
  */
-
-// PWM Duty cycle is CCPRxL (low byte) and CCPRxH (high byte)
 
 #include <xc.h>
 #include <stdint.h>
 #include <pic18f47j13.h>
-#define _XTAL_FREQ 32000000                  // Fosc  frequency for _delay
+
+#define _XTAL_FREQ 32000000     //Fosc frequency for _delay
+
+#define BOARD_NUM 0
+#define BOARD_CHANNELS 6
+
+volatile int board = BOARD_NUM; //Figure out a better way to set this
+volatile int channel = 0;       //Initialize channel to 0. Read from 0 -> 512
+volatile int localChannel = 0;  //Keep track of which channel on the board
+volatile char dmxByte;          //This will hold the raw DMX read
+volatile int channelValues[BOARD_CHANNELS];
 
 void setup(void)
 {
@@ -18,6 +29,8 @@ void setup(void)
     RCSTA1bits.SPEN = 1;        //Enable Serial Port Receive
     TRISCbits.TRISC7 = 1;       //Enable input
     PIE1bits.RC1IE = 1;         //Enable interrupts
+    BAUDCON1bits.BRG16 = 1;     //Enable 16-bit baudrate
+    SPBRG1 = 3;                 //250k=16000000/(16*(3+1)) Set baudrate 250kHz
     
     //Set up output
     CCP4CONbits.CCP4M = 0b1100;
@@ -36,25 +49,57 @@ void setup(void)
     TRISBbits.TRISB5 = 0;
     TRISBbits.TRISB4 = 0;
     
-    //Setup timer and PWM Frequencies
-    T2CON = 0b00000100;     // Enable TMR2 with prescaler = 1
-    PR2 = 249;   // PWM period = (PR2+1) * prescaler * Tcy = 1ms
-    CCPR1L = 25; // pulse width = CCPR1L * prescaler * Tcy = 100us
+    //Set up timer and PWM Frequencies
+    //PWM Duty cycle is CCPRxL (low byte) and CCPRxH (high byte)
+    T2CON = 0b00000100;         //Enable TMR2 with prescaler = 1
+    PR2 = 249;                  //PWM period = (PR2+1) * prescaler * Tcy = 1ms
+    CCPR1L = 25;                //pulse width = CCPR1L * prescaler * Tcy = 100us
 }
 
 void interrupt ISR() {
     if(PIR1bits.RC1IF == 1) {   //If interrupt on UEART receive
         PIR1bits.RC1IF = 0;     //Reset it to 0.
+        if(RCSTA1bits.FERR) {
+            channel = 0;        //If end of signal
+            localChannel = 0;   //Keep track of which channel in the board
+            dmxByte = RCREG1;   //Start reading again
+        }
+        else {
+            channel++;          //Next channel
+            dmxByte = RCREG1;   //Read data
+        }
+        //If we're in the right range of values for this board
+        if((channel >= (16*board)) && (localChannel < BOARD_NUM)) {
+            channelValues[localChannel] = dmxByte;  //Keep track of DMX data
+            localChannel++;                         //The go to the next channel
+        }
     }
+}
+
+void write() {
+    //Do the actual writing to the ports.
+    //Since
+    CCPR4H = channelValues[0];
+    CCPR5H = channelValues[1];
+    CCPR6H = channelValues[2];
+    CCPR7H = channelValues[3];
+    CCPR8H = channelValues[4];
+    CCPR9H = channelValues[5];
 }
 
 void main(void)
 {
-    int DMX_ch0, DMX_ch1, DMX_ch2, DMX_ch3, 
-            DMX_ch4, DMX_ch5, DMX_ch6;
     
     setup();
 
+    //Start everything as off
+    CCPR4H = 0;
+    CCPR5H = 0;
+    CCPR6H = 0;
+    CCPR7H = 0;
+    CCPR8H = 0;
+    CCPR9H = 0;
+    
     while(1)
     {
         
@@ -64,7 +109,6 @@ void main(void)
         CCPR7L = 150;
         CCPR8L = 200;
         CCPR9L = 230;
-        CCPR10L = 255;
         
         __delay_ms(5);
     }
