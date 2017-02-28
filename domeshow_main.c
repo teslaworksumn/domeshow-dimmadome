32dbb5428287d31411500331cc5d147bebd38742
Ian Smith <smit7356@umn.edu>
2017-02-28 12:10:08 -0600

    Progress. Still not working, not entirely sure why.

--- domeshow_main.c
+++ domeshow_main.c
@@ -12,16 +12,18 @@
 #include <stdint.h>
 #include <pic18f47j13.h>
 
+#pragma config XINST = OFF      //Allows use of MPLab simulator for debugging
+
 #define _XTAL_FREQ 32000000     //Fosc frequency for _delay
 
-#define BOARD_NUM 0
-#define BOARD_CHANNELS 6
+#define BOARD_NUM 0             //Board/Chip number
+#define BOARD_CHANNELS 6        //Channels per chip
 
 volatile int board = BOARD_NUM; //Figure out a better way to set this
 volatile int channel = 0;       //Initialize channel to 0. Read from 0 -> 512
 volatile int localChannel = 0;  //Keep track of which channel on the board
 volatile char dmxByte;          //This will hold the raw DMX read
-volatile int channelValues[BOARD_CHANNELS];
+volatile uint8_t channelValues[BOARD_CHANNELS];
 
 void setup(void)
 {
@@ -30,7 +32,8 @@ void setup(void)
     TRISCbits.TRISC7 = 1;       //Enable input
     PIE1bits.RC1IE = 1;         //Enable interrupts
     BAUDCON1bits.BRG16 = 1;     //Enable 16-bit baudrate
-    SPBRG1 = 3;                 //250k=16000000/(16*(3+1)) Set baudrate 250kHz
+    SPBRG1 = 3;                 //250k=16000000/(16*(3+1)) Set baudrate 250kbps
+                                //or is Fosc actually 32M??
     
     //Set up output
     CCP4CONbits.CCP4M = 0b1100;
@@ -42,7 +45,7 @@ void setup(void)
     //CCP10CONbits.CCP10M = 0b1100; //Disable for DMX Receive
     
     TRISCbits.TRISC1 = 0;
-    //TRISCbits.TRISC7 = 0; //Disable for DMX Receive
+    //TRISCbits.TRISC7 = 1;       //Input for DMX Receive. Done above.
     TRISCbits.TRISC6 = 0;
     TRISBbits.TRISB7 = 0;
     TRISBbits.TRISB6 = 0;
@@ -52,8 +55,9 @@ void setup(void)
     //Set up timer and PWM Frequencies
     //PWM Duty cycle is CCPRxL (low byte) and CCPRxH (high byte)
     T2CON = 0b00000100;         //Enable TMR2 with prescaler = 1
-    PR2 = 254;                  //PWM period = (PR2+1) * prescaler * Tcy = 1ms
-    CCPR1L = 25;                //pulse width = CCPR1L * prescaler * Tcy = 100us
+    //TODO: Fix these?
+    PR2 = 254;                  //PWM period = (PR2+1) * prescaler * Tcy * 4
+    CCPR1L = 25;                //pulse width = CCPR1L * prescaler * Tcy * 4
 }
 
 void interrupt ISR() {
@@ -69,7 +73,7 @@ void interrupt ISR() {
             dmxByte = RCREG1;   //Read data
         }
         //If we're in the right range of values for this board
-        if((channel >= (16*board)) && (localChannel < BOARD_NUM)) {
+        if((channel >= (BOARD_CHANNELS * board)) && (localChannel < BOARD_CHANNELS)) {
             channelValues[localChannel] = dmxByte;  //Keep track of DMX data
             localChannel++;                         //The go to the next channel
         }
@@ -78,13 +82,22 @@ void interrupt ISR() {
 
 void write() {
     //Do the actual writing to the ports.
-    //Since
-    CCPR4H = channelValues[0];
-    CCPR5H = channelValues[1];
-    CCPR6H = channelValues[2];
-    CCPR7H = channelValues[3];
-    CCPR8H = channelValues[4];
-    CCPR9H = channelValues[5];
+    //TODO: Fix scaling
+    
+    //Test
+    channelValues[0] = 90;
+    channelValues[1] = 90;
+    channelValues[2] = 90;
+    channelValues[3] = 90;
+    channelValues[4] = 90;
+    channelValues[5] = 90;
+    
+    CCPR4L = channelValues[0];      //RP7
+    CCPR5L = channelValues[1];      //RP8
+    CCPR6L = channelValues[2];      //RP9
+    CCPR7L = channelValues[3];      //RP10
+    CCPR8L = channelValues[4];      //RP12
+    CCPR9L = channelValues[5];      //RP17
 }
 
 void main(void)
@@ -103,12 +116,13 @@ void main(void)
     while(1)
     {
         
-        CCPR4L = 0;
-        CCPR5L = 50;
-        CCPR6L = 100;
-        CCPR7L = 150;
-        CCPR8L = 200;
-        CCPR9L = 230;
+//        CCPR4L = 0;
+//        CCPR5L = 50;
+//        CCPR6L = 100;
+//        CCPR7L = 150;
+//        CCPR8L = 200;
+//        CCPR9L = 230;
+        write();
         
         __delay_ms(5);
     }
