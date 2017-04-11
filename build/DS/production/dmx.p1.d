bce851d1fb522064da11de204f4581597eb9e425
Ryan Fredlund <rfredlund13@gmail.com>
2017-04-11 00:01:13 -0500

    Removed built files

--- build/DS/production/dmx.p1.d
+++
@@ -1,5 +0,0 @@
- build/DS/production/dmx.d  \
- build/DS/production/dmx.p1:  \
- dmx.c  \
-dmx.h  \
-dmxconfig.h 
--- build/DS/production/dome_main.p1.d
+++
@@ -1,6 +0,0 @@
- build/DS/production/dome_main.d  \
- build/DS/production/dome_main.p1:  \
- dome_main.c  \
-dmx.h  \
-domeshow_lib.h  \
-dmxconfig.h 
